921abd2fa0adbef89a140a6480f02f05eca30c3c
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-03 19:22:35 +0600

    cors added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const app = express();
+const port =process.env.PORT || 5000;
+const cors =require('cors');
+
+const chef = require('./data/chef.json');
+
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send('dragon is running')
+});
+
+app.get('/chef',(req, res)=>{
+    res.send(chef)
+})
+
+app.listen(port,()=>{
+    console.log(`cheif/recipie api is running on port: ${port}`)
+})
