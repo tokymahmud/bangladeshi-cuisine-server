da3b30134c39662a32e0396aaaaa3b04a8bb8e3d
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-04 13:59:13 +0600

    req response updated id wise

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ app.use(cors());
 app.get('/',(req,res)=>{
     res.send('chef is running')
 });
-
-app.get('/chef',(req, res)=>{
+app.get('/chef',(req,res)=>{
     res.send(chef)
+});
+
+app.get('/chef/:id',(req, res)=>{
+    const id =req.params.id;
+    const result = chef.find((data)=>data.id==id)
+    res.send(result)
+    
 })
 
 app.listen(port,()=>{
